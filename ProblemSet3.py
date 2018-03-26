97102f16f92440e6d97c5ee212cad7123c59b1af
Nebiyou S Abebe <nebexs@gmail.com>
2018-03-26 13:16:07 -0700

    Update ProblemSet3.py

--- ProblemSet3.py
+++ ProblemSet3.py
@@ -205,8 +205,8 @@ in the first file.  If line is the line that you've read into your program, use
 line.strip("\n") to strip away the invisible newline character at the end of
 each line.  Otherwise, your count will be one higher than the autograder's. 
 Note that since this is a program running from the Command Window (or terminal
-or cmd.exe), it won't be runnable as our usual functions are by entering
-Shift-Enter.  You should use the File menu in Spyder to create you own file.
+or cmd.exe), it won't be runnable as our usual functions are by entering Shift-Enter.  
+You should use the File menu in Spyder to create you own file.
 But, if you prefer, there is a starter file called problem3_6starter.py.
 
 Here is a run of my solution program using the HumptyDumpty.txt file. The run
